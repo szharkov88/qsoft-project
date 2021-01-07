f1319ed7326650989dbf83a41d0c866c4ee56a8f
szharkov88 <sergeyzharkov9@mail.ru>
2021-01-07 20:38:06 +0300

    Заработали иконки в навигации

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,37 +1,36 @@
-import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import Feather from 'react-native-vector-icons/Feather'
-import MainScreen from '../screens/MainScreen'
-import FavoritesScreen from '../screens/FavoritesScreen'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
+import MainScreen from '../screens/MainScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigation() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
 
-                        if (route.name === 'Home') {
-                            iconName = 'home'
-                        } else if (route.name === 'Liked') {
-                            iconName = 'heart'
-                        }
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Liked') {
+              iconName = 'heart';
+            }
 
-                        return <Feather name={iconName} size={size} color={color}/>;
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'tomato',
-                    inactiveTintColor: 'gray',
-                }}
-            >
-                <Tab.Screen name="Home" component={MainScreen} />
-                <Tab.Screen name="Liked" component={FavoritesScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+            return <Feather name={iconName} size={35} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#5533EA',
+          inactiveTintColor: '#BDBDBD',
+        }}>
+        <Tab.Screen name="Home" component={MainScreen} />
+        <Tab.Screen name="Liked" component={FavoritesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
