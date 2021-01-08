6d83eab8f074bea668c9e20846fb7745b3019b9c
szharkov88 <sergeyzharkov9@mail.ru>
2021-01-08 23:28:32 +0300

    Добавили страницу с избранными постами

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
 import NavBar from '../components/NavBar';
-import {THEME} from "../theme";
+import {THEME} from '../theme';
+import {DATA} from '../../assets/data/data';
+import {Post} from '../components/Post';
+import {LoadMoreButton} from '../components/ui/LoadMoreButton';
 
-export default function MainScreen() {
+export default function MainScreen({navigation}) {
+  const [offset, setOffset] = useState(7);
+  const [loading, setLoading] = useState(false);
+  const [fullLoadData, setFullLoadData] = useState(false);
+
+  const dataList = DATA.filter(post => post.liked).slice(0, offset);
+
+  const getData = () => {
+    console.log('Getting Data...');
+    setLoading(true);
+    setOffset(offset + 8);
+    if (DATA.filter(post => post.liked).length - dataList.length <= 7) {
+      setFullLoadData(true);
+    }
+    setLoading(false);
+  };
+
+  const renderFooter = () => {
+    if (!fullLoadData) {
+      return <LoadMoreButton onPress={getData} loading={loading} />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <NavBar />
-      <Text>Favorites</Text>
+      <FlatList
+        style={styles.listWrapper}
+        data={dataList}
+        keyExtractor={(post) => post.id.toString()}
+        renderItem={({item}) => <Post post={item} />}
+        ListFooterComponent={renderFooter}
+      />
     </View>
   );
 }
@@ -15,7 +48,11 @@ export default function MainScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     marginHorizontal: 20,
-    backgroundColor: THEME.BC_COLOR
+    backgroundColor: THEME.BC_COLOR,
+  },
+  listWrapper: {
+    marginTop: 10,
   },
 });
